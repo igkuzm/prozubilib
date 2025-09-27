a4174f0ef2a294e9e951daa5ae38e68c522553a1
Igor Sementsov <kuzmich@power-mac-g5-igor-sementsov.local>
2025-09-27 19:11:38 +1100

    mkb

--- mkb.h
+++ mkb.h
@@ -71,6 +71,92 @@ _mkb_new(prozubi_t *p)
 }
 
 static void
+_prozubi_mkb_get_child(
+		prozubi_t *p,
+		sqlite3 *db,	
+		int parent_id,
+		void *user_data,
+		void *parent,
+		void * (*callback)(
+			void * user_data,
+			void * parent,
+			mkb_t *ptr
+			)
+		)
+{
+	/* get children */
+	sqlite3_stmt *stmt_c;
+	
+	/* create SQL string */
+	char SQL_c[BUFSIZ] = "SELECT ";
+	
+	#define MKB_COLUMN_INT(title) strcat(SQL_c, #title); strcat(SQL_c, ", "); 
+	#define MKB_COLUMN_TEX(title) strcat(SQL_c, #title); strcat(SQL_c, ", "); 
+	MKB_COLUMNS
+	#undef MKB_COLUMN_INT
+	#undef MKB_COLUMN_TEX
+
+	sprintf(SQL_c, "%s 1 FROM %s WHERE parent = %d", SQL_c, MKB_TABL, parent_id);	
+
+	if (p->on_log)
+		p->on_log(p->on_log_data, STR("SQL: %s\n", SQL_c));		
+	
+	/* start SQLite request */
+	if(sqlite3_prepare_v2(db, SQL_c, -1, &stmt_c, NULL)){
+		if (p->on_error)
+			p->on_error(p->on_error_data,			
+			STR_ERR("sqlite3_prepare_v2: %s: %s", SQL_c, sqlite3_errmsg(db)));	
+		sqlite3_close(db);
+		return;
+	};
+
+	while (sqlite3_step(stmt_c) != SQLITE_DONE) {
+		mkb_t *c = _mkb_new(p);
+		if (!c)
+			break;
+		
+		/* iterate columns */
+		int i;
+		for (i = 0; i < MKB_COLS_NUM; ++i) {
+			/* handle values */
+			switch (i) {
+
+#define MKB_COLUMN_TEX(title) \
+				case MKB_##title:\
+				{\
+					size_t len = sqlite3_column_bytes(stmt_c, i);\
+					const unsigned char *value = sqlite3_column_text(stmt_c, i);\
+					c->title = strdup((char*)value);\
+					c->title[len] = 0;\
+					break;\
+				};				
+#define MKB_COLUMN_INT(title) \
+				case MKB_##title:\
+				{\
+					int value = sqlite3_column_int(stmt_c, i);\
+					c->title = value;\
+					break;\
+				};				
+
+			MKB_COLUMNS
+
+#undef MKB_COLUMN_TEX			
+#undef MKB_COLUMN_INT			
+
+				default:
+					break;					
+			}
+		}		
+		/* callback */
+		parent = callback(user_data, parent, c);
+
+		// loop
+		_prozubi_mkb_get_child(p, db, c->iD, user_data, parent, callback);
+	}	
+	sqlite3_finalize(stmt_c);
+}
+
+static void
 prozubi_mkb_foreach(
 		prozubi_t *p,
 		const char *predicate,
@@ -173,73 +259,8 @@ prozubi_mkb_foreach(
 		/* callback */
 		void *parent = callback(user_data, NULL, c);
 
-		/* get children */
-		sqlite3_stmt *stmt_c;
-		
-		/* create SQL string */
-		char SQL_c[BUFSIZ] = "SELECT ";
-		
-		#define MKB_COLUMN_INT(title) strcat(SQL_c, #title); strcat(SQL_c, ", "); 
-		#define MKB_COLUMN_TEX(title) strcat(SQL_c, #title); strcat(SQL_c, ", "); 
-		MKB_COLUMNS
-		#undef MKB_COLUMN_INT
-		#undef MKB_COLUMN_TEX
-	
-		sprintf(SQL_c, "%s 1 FROM %s WHERE parent = %d", SQL_c, MKB_TABL, c->iD);	
-
-		if (p->on_log)
-			p->on_log(p->on_log_data, STR("SQL: %s\n", SQL_c));		
-		
-		/* start SQLite request */
-		if(sqlite3_prepare_v2(db, SQL_c, -1, &stmt_c, NULL)){
-			if (p->on_error)
-				p->on_error(p->on_error_data,			
-				STR_ERR("sqlite3_prepare_v2: %s: %s", SQL_c, sqlite3_errmsg(db)));	
-			sqlite3_close(db);
-			return;
-		};
-
-		while (sqlite3_step(stmt_c) != SQLITE_DONE) {
-			mkb_t *c = _mkb_new(p);
-			if (!c)
-				break;
-			
-			/* iterate columns */
-			int i;
-			for (i = 0; i < MKB_COLS_NUM; ++i) {
-				/* handle values */
-				switch (i) {
-
-#define MKB_COLUMN_TEX(title) \
-					case MKB_##title:\
-					{\
-						size_t len = sqlite3_column_bytes(stmt_c, i);\
-						const unsigned char *value = sqlite3_column_text(stmt_c, i);\
-						c->title = strdup((char*)value);\
-						c->title[len] = 0;\
-						break;\
-					};				
-#define MKB_COLUMN_INT(title) \
-					case MKB_##title:\
-					{\
-						int value = sqlite3_column_int(stmt_c, i);\
-						c->title = value;\
-						break;\
-					};				
-
-				MKB_COLUMNS
-
-#undef MKB_COLUMN_TEX			
-#undef MKB_COLUMN_INT			
-
-					default:
-						break;					
-				}
-			}		
-			/* callback */
-			parent = callback(user_data, parent, c);
-		}	
-		sqlite3_finalize(stmt_c);
+		// loop
+		_prozubi_mkb_get_child(p, db, c->iD, user_data, parent, callback);
 	}
 	
 	free(SQL);
